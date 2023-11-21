27014026ecbf251d3f6acbd761d9a7e496cdafc3
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-11-21 20:49:07 +0530

    tenth commit

---
+++ data abstraction/src/Bank1.java
@@ -0,0 +1,14 @@
+interface Bank1
+{
+	void setCustDetails();
+abstract void showCustDetails();
+public static void main(String[] args)
+{
+	
+Bank1.myfun();
+}
+static void myfun()
+{
+	System.out.println("Hello myfun Interface");
+}
+}	
