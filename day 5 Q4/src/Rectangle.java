72b02ab0dba192168532fa28f8314450c1b1d337
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-12-01 23:58:07 +0530

    15 commit

---
+++ day 5 Q4/src/Rectangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class A
+{
+	void area1(int l,int b)
+{
+		System.out.println("Area of rectangle 1 "+(l*b));
+}
+
+void area2(int l,int b)
+{
+System.out.println("Area of rectangle 2 is "+(l*b));}
+}
+
+public class Rectangle 
+{
+public static void main(String[] args) 
+{   A a1=new A();
+	a1.area1(4,5);
+	a1.area2(5,8);
+	}}
