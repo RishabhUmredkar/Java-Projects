427ba950c6b0760343d56390332709f068a76fd2
Rishabh Umredkar <umredkarrishabh@gmail.com>
2024-04-02 23:32:15 +0530

    18 commit

---
+++ employee/src/employee/Connectivity.java
@@ -0,0 +1,19 @@
+package employee;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Connectivity {
+	static String dbname = "rishabh00";
+	static String url="jdbc:mysql://localhost:3306/"+dbname;
+	static String uname="root";
+	static String pass="abc123";
+
+static	Connection getConnection() throws ClassNotFoundException, SQLException
+	{
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection(url,uname,pass);
+		return con;
+	}
+}
